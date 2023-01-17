c3a82925673ea7e54d788719cfc995cf8fc5cdb7
tijanadmi <tijanadmi@yahoo.com>
2023-01-17 23:01:58 +0100

    insert seats

--- app.js
+++ app.js
@@ -51,11 +51,51 @@ const moviesSchema=  new mongoose.Schema({
         plot:  String,
         poster : String,
     repertoires: [
-    {  date: Date, time: String, hall: String, numOfTickets: Number, numOfResTickets: Number }
+    {  date: Date, time: String, hall: String, numOfTickets: Number, numOfResTickets: Number, reservSeats: [String] }
     ]   
 });
 
+
+
 const Movie = mongoose.model("Movie", moviesSchema);
+
+
+
+const repertoiresSchema = new mongoose.Schema({
+    date: Date,
+    time: String,
+    hall: String, 
+    numOfTickets: Number, 
+    numOfResTickets: Number, 
+    reservSeats: [String]
+});
+
+const Repertoire = mongoose.model("Repertoire", repertoiresSchema);
+var myDate = new Date("2023-01-18T16:00:00Z");
+var date2 = new Date("2022-12-15T16:00:00Z");
+const repertoires2 = new Repertoire({
+    date: myDate,
+    time: "16:30",
+    hall: "Sala1", 
+    numOfTickets: 50, 
+    numOfResTickets: 2, 
+    reservSeats: ["A1","A2"]
+});
+
+const movie1 = new Movie({
+    title : "Avatar prvi",
+    duration : "190",
+    genre : "akcija, avantura, nauДЌna-fantastika",
+    directors : "James Cameron",
+    actors : "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang, Cliff Curtis",
+    screening : date2,
+    plot : "DЕѕejk Sali Еѕivi sa svojom porodicom na planeti Pandori. Kada se vrati poznati neprijatelj koji pokuЕЎava da ih uniЕЎti, DЕѕejk i Nejtiri se udruЕѕuju sa vojskom Navi naroda kako bi zaЕЎtitili svoju planetu.",
+    poster : "https://img.cineplexx.rs/media/rs/inc/movies_licences/avatar_223_1.jpg",
+    repertoires : [repertoires2]
+
+});
+
+//movie1.save();
 
 const hallsSchema=  new mongoose.Schema({
     name: String,
@@ -69,11 +109,20 @@ const usersSchema=  new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
-    secrets: [String]
+    name: String,
+    reservations: [
+        {  date: Date, time: String, hall: String, reservSeats: [String] }
+        ]   
 });
 
+/*const reservationsSchema=  new mongoose.Schema({
+    date: Date,
+    time: String,
+    hall: String, 
+    reservSeats: [String]
+});
 
-
+const Reservation = mongoose.model("Reservation", reservationsSchema);*/
 
 
 
@@ -292,11 +341,12 @@ app.get("/logout", function(req, res, next) {
 
   });
 
-  app.get("/reservation/:date/:time/:hall", function(req, res){
+  app.get("/reservation/:date/:time/:hall/:movieId/:resId", function(req, res){
     let optionsm = {month: "2-digit"}
     const requestedDate = req.params.date;
     const requestedTime = req.params.time;
     const requestedHall = req.params.hall;
+    const requestedMovieId = req.params.movieId;
     
     
     
@@ -304,7 +354,7 @@ app.get("/logout", function(req, res, next) {
         Hall.findOne({name: requestedHall}, function(err, foundHall){
             if (foundHall){
                 console.log(foundHall);
-                res.render("reservation",{username: "Tijana", date: requestedDate, time: requestedTime, hall:foundHall });
+                res.render("reservation",{username: "Tijana", movieId: requestedMovieId, date: requestedDate, time: requestedTime, hall:foundHall, paramHall: req.params.hall, paramDate: req.params.date, resId: req.params.resId });
             } else {
                 res.send("No hall matching that requestedHall was found.");
             }
@@ -313,6 +363,63 @@ app.get("/logout", function(req, res, next) {
     } else {
         res.redirect("/login");
     }
+});
+
+app.post("/reservation/:date/:time/:hall/:movieId/:resId", function(req, res){
+    var res = req.body.add;
+    console.log(typeof res);
+    var typeOut = typeof res;
+    var niz = [];
+    if (typeOut ==="undefined"){
+        console.log("Nije nista izabrano!");
+    } else if (typeOut ==="string"){
+        num = 1;
+        niz.push(res);
+        console.log(num);
+    } else {
+        num = res.length;
+        niz = res;
+        console.log(num);
+    }
+   
+        const requestedDate = req.params.date;
+        const requestedTime = req.params.time;
+        const requestedHall = req.params.hall;
+        const requestedMovieId = req.params.movieId;
+        console.log(requestedMovieId);
+        var a = requestedDate.split('.');
+        var start = new Date(a[2] + "-" + a[1] + "-" + a[0]).toISOString();
+        
+        Movie.findOne({'repertoires._id':req.params.resId},{reservSeats:1,  repertoires: { $elemMatch:{ _id:req.params.resId } }}, function (err, foundMovie) {
+            if (err){
+                console.log(err);
+            }
+            else{
+                console.log(foundMovie.repertoires[0].numOfTickets);
+                console.log(foundMovie.repertoires[0].numOfResTickets);
+                console.log(foundMovie.repertoires[0].reservSeats);
+                numberOfT = foundMovie.repertoires[0].numOfTickets;
+                numberOfRT = foundMovie.repertoires[0].numOfResTickets;
+                if (numberOfT < numberOfRT + num){
+                    console.log("Karte su rasprodate! Probajte sa manjim brojem");
+                } else {
+                    var niz1 = foundMovie.repertoires[0].reservSeats;
+                    var niz2 = niz1.concat(niz);
+                    console.log(niz2);
+                    Movie.findOneAndUpdate({'repertoires._id': req.params.resId}, { "repertoires.$.reservSeats" : niz2 , "repertoires.$.numOfResTickets" : numberOfRT + num}, function (err, foundMovie) {
+                        if (err){
+                            console.log(err);
+                        } else {
+                            console.log("Uspesno!");
+                        }});
+                }
+                /*foundMovie.repertoires.forEach(function(repertoire){ 
+                    console.log(repertoire.date);
+                    console.log(repertoire.reservSeats);
+                    
+                    });*/
+            }
+        });
 });
 
 app.listen(3000, function() {
